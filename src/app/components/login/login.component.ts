84426d2095ac7fa379985dc7ef07f38637bbc9c9
julian vargas <julian.vargas@grupokonecta.com>
2020-02-13 08:44:35 -0500

    close issue#3

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,14 +1,20 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { AutenticateApiService } from "../../api/autenticate-api.service";
 import { JwtHelperService } from '@auth0/angular-jwt';
 import { Router } from '@angular/router';
+
+type httpError = {
+    status: number;
+    statusText: string
+}
 
 @Component({
     selector: 'app-login',
     templateUrl: './login.component.html',
     styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, AfterViewInit {
+
 
     @ViewChild('usuario', { static: false }) usuario: ElementRef;
     @ViewChild('password', { static: false }) password: ElementRef;
@@ -17,24 +23,46 @@ export class LoginComponent implements OnInit {
         private autenticateApi: AutenticateApiService,
         private router: Router) { }
 
-    ngOnInit() { }
+    ngOnInit() {  }
+    
+    ngAfterViewInit() {
+        setTimeout(()=>{
+            this.usuario.nativeElement.focus();
+        },0);  
+    }
+
+    isError = false
+
+    isLoading = false
 
     login(): void {
-        if (this.usuario.nativeElement.value && this.password.nativeElement.value) {
-            this.autenticateApi.login(this.usuario.nativeElement.value, this.password.nativeElement.value)
-                .subscribe(autenticated => {
-                    if (autenticated) {
-                        // window.setTimeout(()=>{
-                        //     localStorage.removeItem('token')
-                        //     this.router.navigate(['login'])
-                        // },
-                        //     (new JwtHelperService()).getTokenExpirationDate(localStorage.getItem("token")).getTime()
-                        // )
+        if( this.usuario.nativeElement.value && this.password.nativeElement.value ){
+            this.isError = false
 
-                        this.router.navigate(['/app']);
-                    }
-                })
+            this.isLoading = true
+
+            if (this.usuario.nativeElement.value && this.password.nativeElement.value) {
+                this.autenticateApi.login(this.usuario.nativeElement.value, this.password.nativeElement.value)
+                    .subscribe(autenticated => {
+                        if (autenticated) {                  
+                            this.router.navigate(['/app']);
+                        } else {
+                            this.isError = true
+                        }
+
+                        this.isLoading = false
+
+                    },(error:httpError) => {
+                        if(error.statusText == 'Unauthorized'){
+                            this.isError = true
+                            this.isLoading = false
+                        }                            
+
+                    })
+            }
+
         }
+
     }
 
     enter(event:KeyboardEvent){
